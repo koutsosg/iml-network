39964a6207cc3028b8002f26e5c8f330e1c16ebb
koutsosg <g.koutsos123@gmail.com>
2024-10-14 22:04:12 +0300

    correct the eslint error

--- www/app/page.tsx
+++ www/app/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { FRIENDS_QUERY } from "../utils/urlq/queries";
 import { fetchGraphQL } from "../utils/urlq/fetchGraphQL";
-
+type Friend = {
+  name: string;
+};
 export default async function Home() {
   const data = await fetchGraphQL(FRIENDS_QUERY);
 
@@ -12,7 +14,7 @@ export default async function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        {data.friends.map((friend: any) => (
+        {data.friends.map((friend: Friend) => (
           <div className="capitalize " key={friend.name}>
             {friend.name}
           </div>
