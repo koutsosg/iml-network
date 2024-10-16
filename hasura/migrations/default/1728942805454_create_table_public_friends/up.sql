5644818486a45a2eb2f7a3713667be3fc564e106
koutsosg <g.koutsos123@gmail.com>
2024-10-17 01:51:47 +0300

    reinstall hasura

--- hasura/migrations/default/1728942805454_create_table_public_friends/up.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE "public"."friends" ("id" serial NOT NULL, "name" text NOT NULL, PRIMARY KEY ("id") , UNIQUE ("id"));
--- hasura/migrations/default/1728943151479_init/up.sql
+++
@@ -1,4 +0,0 @@
-INSERT into friends (name) VALUES ('George');
-INSERT into friends (name) VALUES ('Lady');
-INSERT into friends (name) VALUES ('Hajda');
-INSERT into friends (name) VALUES ('Lenakolokitha');
